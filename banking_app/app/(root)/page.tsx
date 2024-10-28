3f36270d14c0f996a214fc2dbea40adc1a17a0ac
Furkan Erdoğuş <107814668+PLADSS@users.noreply.github.com>
2024-10-28 13:06:38 +0300

    02
    02

--- banking_app/app/(root)/page.tsx
+++ banking_app/app/(root)/page.tsx
@@ -1,8 +1,30 @@
+import TotalBalanceBox from '@/components/TotalBalanceBox'
+import HeaderBox from '@/components/ui/HeaderBox'
 import React from 'react'
 
 const Home = () => {
+
+    const loggedIn = {firstName : "Furkan"}
+
+
   return (
-    <div>Home</div>
+    <section className='home'>
+        <div className="home-content">
+            <header className='home-header'>
+                <HeaderBox 
+                 type="greeting"
+                 title="Welcome"
+                 user={loggedIn?.firstName || "Guest"}
+                 subtext="Acces and Manage your account"
+                />
+
+                <TotalBalanceBox
+                accounts ={[]}
+                totalBanks={1} 
+                totalCurrentBalance={1250.35}/>
+            </header>
+        </div>
+    </section>
   )
 }
 
